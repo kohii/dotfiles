7a94b95da97475ee81a4af20f937893ddf8e1b20
kohii <kohii.tokyo@gmail.com>
2023-08-15 15:08:17 +0900

    add mackup to sync raycast

--- install.sh
+++ install.sh
@@ -1,29 +1,13 @@
 #!/bin/bash
 
 install_dotfiles() {
-	local dotfiles=(.ideavimrc, .config/nvim)
+	local dotfiles=(.ideavimrc .mackup.cfg .config/nvim)
 	for file in "${dotfiles[@]}"; do
 		ln -svf ~/dotfiles/$file ~/$file
 	done
 }
 
-install_configs() {
-	local src=~/dotfiles/.config/
-	local dst=~/.config/
-
-	# ソースディレクトリ内のすべてのファイルとディレクトリに対して繰り返し処理
-	find "$src" -type f -or -type d | while read -r file; do
-	    # ソースディレクトリの接頭辞を削除してターゲットへのパスを取得
-	    target="${dst}${file#$src}"
-
-	    # ターゲットの親ディレクトリを作成（すでに存在する場合はスキップ）
-	    mkdir -p "$(dirname "$target")"
-
-	    # シンボリックリンクを作成（すでに存在する場合は上書き）
-	    ln -svf "$file" "$target"
-	done
-}
-
-
 install_dotfiles
-#install_configs
+
+mackup backup
+
