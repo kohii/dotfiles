df119a7572ea9462a32885456826a40b0ccbd197
kohii <kohii.tokyo@gmail.com>
2023-08-16 20:31:33 +0900

    add config for vscode

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -525,3 +525,7 @@ cmp.setup {
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
+
+if vim.g.vscode then
+  require("vscode")
+end
---
+++ .config/nvim/lua/vscode.lua
@@ -0,0 +1,2 @@
+-- https://github.com/vscode-neovim/vscode-neovim/issues/298
+vim.opt.clipboard:append("unnamedplus")
