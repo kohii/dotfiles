0b57e5b9d2eabcca1b1307f098050968fcc76744
kohii <kohii.tokyo@gmail.com>
2024-03-14 21:47:16 +0900

    improve installation

--- install.sh
+++ install.sh
@@ -1,17 +1,42 @@
 #!/bin/bash
 
-install_dotfiles() {
-	local dotfiles=(.ideavimrc .mackup.cfg .gitconfig .gitignore_global .zshrc .config/nvim Brewfile .config/starship.toml)
-	for file in "${dotfiles[@]}"; do
-		if [ -d ~/dotfiles/$file ]; then
-			ln -svf ~/dotfiles/$file/ ~/$file
-		else
-			ln -svf ~/dotfiles/$file ~/$file
-		fi
-	done
+dotfiles_dir=~/dotfiles
+dotfiles_list_file=~/dotfiles/dotfiles.txt
+
+create_symlink() {
+    local source=$1
+    local target=$2
+
+    if [ -e "$target" ]; then
+        if [ -L "$target" ]; then
+            ln -svf "$source" "$target"
+        else
+            echo "Error: $target already exists and is not a symlink."
+        fi
+    else
+        ln -sv "$source" "$target"
+    fi
 }
 
-install_dotfiles
+install_dotfiles() {
+    while IFS= read -r file; do
+        source="$dotfiles_dir/$file"
+        target="$HOME/$file"
+
+        if [ -d "$source" ]; then
+            create_symlink "$source/" "$target"
+        else
+            create_symlink "$source" "$target"
+        fi
+    done < "$dotfiles_list_file"
+}
+
+if [ -f "$dotfiles_list_file" ]; then
+    install_dotfiles
+else
+    echo "Error: $dotfiles_list_file not found."
+    exit 1
+fi
 
 chmod +x ./configure_macos.sh
 ./configure_macos.sh
