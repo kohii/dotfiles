866bf84e1c5fa17573e5bfb2da381221bebbcd28
kohii <kohii.tokyo@gmail.com>
2024-05-18 13:36:52 +0900

    tweak nvim options

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -4,3 +4,16 @@ require "nvchad.options"
 
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
+
+-- Sets how neovim will display certain whitespace characters in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+vim.opt.list = true
+vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+-- Preview substitutions live, as you type!
+vim.opt.inccommand = 'split'
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.opt.scrolloff = 5
+
