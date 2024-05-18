1c15336ee839ecb5c8116629007edcf508caeba3
kohii <kohii.tokyo@gmail.com>
2024-05-18 14:08:14 +0900

    setup kitty-scrollback.nvim

---
+++ .config/nvim/lua/plugins/kitty.lua
@@ -0,0 +1,16 @@
+local plugins = {
+  {
+    'mikesmithgh/kitty-scrollback.nvim',
+    enabled = true,
+    lazy = not vim.g.vscode,
+    cmd = { 'KittyScrollbackGenerateKittens', 'KittyScrollbackCheckHealth' },
+    event = { 'User KittyScrollbackLaunch' },
+    -- version = '*', -- latest stable version, may have breaking changes if major version changed
+    -- version = '^4.0.0', -- pin major version, include fixes and features that do not have breaking changes
+    config = function()
+      require('kitty-scrollback').setup()
+    end,
+  }
+}
+
+return plugins
