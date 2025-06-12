43b009ea02559219f3f46a7912391ab5e81f5da7
kohii <kohii.tokyo@gmail.com>
2025-06-12 22:35:57 +0900

    tweak: accept suggestion by C-g

--- .config/nvim/lua/plugins/copilot.lua
+++ .config/nvim/lua/plugins/copilot.lua
@@ -5,7 +5,8 @@ local plugins = {
   config = function()  -- Mapping tab is already used by NvChad
     vim.g.copilot_no_tab_map = true;
     vim.g.copilot_assume_mapped = true;
-    vim.api.nvim_set_keymap("i", "<D-j>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+    vim.api.nvim_set_keymap("i", "<C-g>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+
     -- vim.g.copilot_tab_fallback = "";
   -- The mapping is set to other key, see custom/lua/mappings
   -- or run <leader>ch to see copilot mapping section
